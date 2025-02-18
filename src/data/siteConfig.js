cf422163e968da813f36f920b34cc204dc00a774
MatchaOnMuffins <zeri@umich.edu>
2025-02-18 15:14:35 -0500

    Convert website to Astro

---
+++ src/data/siteConfig.js
@@ -0,0 +1,10 @@
+const siteConfig = {
+    siteTitle: "MSBT Team Page",
+    siteTitleShort: "MSBT Team Page",
+    siteTitleAlt: "MSBT Team Page",
+    siteDescription: "MSBT Team Page",
+    siteUrl: "https://websites.umich.edu/~umichsynbio/",
+}
+
+export default siteConfig
+
