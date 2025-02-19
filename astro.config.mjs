006c798e033ecc646d078488d5cc4586bbd9f9dc
MatchaOnMuffins <zeri@umich.edu>
2025-02-19 14:06:29 -0500

    Refactor assets and set base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'astro/config';
+import { env } from 'process';
 
 // https://astro.build/config
 export default defineConfig({
+  base: "Website/",
   vite: {
     plugins: [tailwindcss()],
   },
